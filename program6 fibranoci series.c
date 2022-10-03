c59f9ce39fbdec6564f0e31ae18f14022210ec6a
shanwazshaik <113328105+shanwazshaik@users.noreply.github.com>
2022-10-03 22:39:43 +0530

    Add files via upload

---
+++ program6 fibranoci series.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fibbonacci(int n) {
+ if(n == 0){
+ return 0;
+ } else if(n == 1) {
+ return 1;
+ } else {
+ return (fibbonacci(n-1) + fibbonacci(n-2));
+ }
+}
+int main() {
+ int n;
+ int i;
+ printf("Sk shanwaz\n191911494\n ");
+ printf("enter the number:");
+ scanf("%d",&n);
+ printf("Fibbonacci of %d: " , n);
+
+ for(i = 0;i<n;i++) {
+ printf("%d ",fibbonacci(i));
+ }
+}
